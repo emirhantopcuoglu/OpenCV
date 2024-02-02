080e752c626030b6172a1e08312a41f7865ea389
emirhantopcuoglu <emirhantopcuoglu386@gmail.com>
2024-02-02 23:17:35 +0300

    size, dtype and shape functions

--- opencv.py
+++ opencv.py
@@ -7,6 +7,14 @@ cv2.imshow("Landscape",image) # imshow fonksiyonu ile goruntu gosterilir
 
 cv2.imwrite("new_landscape.png",image) # Goruntuyu kaydetmek icin kullanilir
 
+print(image) # Gorunutunun piksel degerlerini (matrisleri) yazdirir
+
+print(image.size) # Goruntunun piksel sayisini yazdirir
+
+print(image.dtype) # Bir NumPy dizisinin veri tipini dondurur
+
+print(image.shape) # Goruntunun genislik, yukseklik ve renk kanal sayisini dondurur
+
 cv2.waitKey(0) # Kullanici bir tusa basana kadar bekleyerek pencereyi acik tutar
 
 cv2.destroyAllWindows() # Pencereyi kapatir
